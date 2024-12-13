21d12a0ba30bdf3ad2a2e6907c604918171d3433
Ferta <fertaprogram@gmail.com>
2024-12-13 15:43:35 +0700

    db conf

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const mysql2 = require('mysql2');
 
 const db = new Sequelize('sql12751772', 'sql12751772', 'I1qZyAlyAF', {
     host : 'sql12.freemysqlhosting.net',
